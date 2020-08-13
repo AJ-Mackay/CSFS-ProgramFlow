80d9f97e229f3e69901c5d9dc172fd0178f24a9a
Ruiner85 <paul.m.mackay85@gmail.com>
2020-08-13 14:41:30 +0100

    initial commit

---
+++ Conditionals/Conditionals/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your age? ");
+            string ageAsString = Console.ReadLine();
+            int age = int.Parse(ageAsString);
+
+            if (age > 21)
+            {
+                Console.WriteLine("You are an adult");
+            }
+            else
+            {
+                Console.WriteLine("You are not yet a legal adult.");
+            }
+        }
+    }
+}
